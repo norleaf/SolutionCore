c09f96aae6cde6536e633163c92aa61027bcecd2
Hans <norleaf@gmail.com>
2015-10-30 14:23:52 +0100

    copied new project
    blahblah blah

---
+++ SolutionCore/SolutionCore/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolutionCore.Models
+{
+    public class Contact
+    {
+        public String Name { get; set; }
+        public String Phone { get; set; }
+        public String Email { get; set; }
+    }
+}
