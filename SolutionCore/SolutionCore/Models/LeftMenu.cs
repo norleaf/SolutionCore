dac81720e397ccefc2e90a0e33e189aeeaf62454
Kim K. Schalls <rauko1@gmail.com>
2015-11-09 20:43:32 +0100

    Prøvet at lave et gui
    Har sat lidt dropdown på. Men nu skal jeg lige have lidt hjælp til at
    lave det dynamisk.

---
+++ SolutionCore/SolutionCore/Models/LeftMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolutionCore.Models
+{
+    public class LeftMenu
+    {
+        private List<MenuItem> ListOfMenuItems = new List<MenuItem>();
+
+        public void addItem(MenuItem item)
+        {
+            ListOfMenuItems.Add(item);
+        }
+        
+        public List<MenuItem> get()
+        {
+            return ListOfMenuItems;
+        }
+
+        public void removeItem(MenuItem item)
+        {
+            ListOfMenuItems.Remove(item);
+        }
+    }
+}
